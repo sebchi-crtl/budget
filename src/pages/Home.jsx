45e112415efc34bcb52473032c1c3e97f0dc612f
sebastine <chiemelienwobodo8@gmail.com>
2023-11-27 16:55:04 +0100

    done with the interview

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,59 @@
-// import MainLayout from '../layouts/MainLayout';
-import {  Box } from "@chakra-ui/react";
+
+import {   Container, Text, VStack, Box, Flex, Icon, Input } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <Box>
-      <div className="home">
-        <h1>Welcome to Home </h1>
+    <Container
+      maxW={['md', 'lg']}
+      h="85vh"
+      overflowY='scroll'
+      py='4em'
+    >
+      <div>
+        <Text fontSize='2.4em' fontWeight='600' display="block" 
+          _hover={{
+            color: [ "gray.600"]
+          }}
+        >
+          Create new budget
+        </Text>
       </div>
-    </Box>
+      <VStack 
+      spacing={4}
+      align='stretch'
+      mt={10}
+      >
+        <Box>
+          <Flex
+            gap={[2, 3]}
+            direction={["row"]}
+            align="center"
+          >
+
+            
+            <Text fontSize={['.9em', '1.2em']} fontWeight='300' display="block" 
+              _hover={{
+                color: [ "gray.600"]
+              }}
+            >
+              How much would you like to spend on each category this month?
+            </Text>
+
+          </Flex>
+          
+        </Box>
+        <Input variant='flushed' placeholder='Amount (in &#8358;)' />
+        
+      </VStack>
+
+      <Text fontSize={['.8em']} fontWeight='300' display="block" mt='17rem' pb='2rem' align='center'
+        _hover={{
+          color: [ "gray.600"]
+        }}
+      >
+        Please click on the budget page for the design
+      </Text>
+    </Container>
   );
 };
 
