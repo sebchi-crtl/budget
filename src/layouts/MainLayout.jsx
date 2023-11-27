130d0fa0869ff99a0b950838f35265a842ae3e55
sebastine <chiemelienwobodo8@gmail.com>
2023-11-27 16:21:06 +0100

    vh

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -9,7 +9,7 @@ const MainLayout = ({ children }) => {
   return (
     <Box
       backgroundColor="#FCFCFC"
-      w="100%" minH="100vh"
+      w="100%" h="147vh"
       style={{ fontFamily: 'CircularStd, sans-serif' }}
     >
       
